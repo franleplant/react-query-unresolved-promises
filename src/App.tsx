71bdbfc1cdaa76c987d7aa094604154e344187ab
Fran Guijarro <franleplant@gmail.com>
2020-06-26 13:10:53 -0300

    format

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import {useQuery, useMutation, queryCache, QueryResult} from 'react-query'
+import React from "react";
+import { useQuery, useMutation, queryCache, QueryResult } from "react-query";
 
 export default function App() {
-  const [mutate, {status}] = useSomeMutation()
-  const loading = status === "loading"
-console.log("mother fucker")
+  const [mutate, { status }] = useSomeMutation();
+  const loading = status === "loading";
+  console.log("mother fucker");
 
   return (
     <div>
@@ -15,25 +15,29 @@ console.log("mother fucker")
   );
 }
 
-
 export function useSomeMutation() {
-  return useMutation<void, undefined>(async () => {
-    await delay()
-  }, {
-    onSuccess: async () => {
-      console.log("onSuccess start")
-      queryCache.setQueryData("notInstantiated", "hello")
-      queryCache.setQueryData("notInstantiated2", "hello")
-      const a = queryCache.refetchQueries("notInstantiated", {force:true})
-      const b = queryCache.refetchQueries("notInstantiated2", {force:true})
-      console.log("promises are never resolved", a, b)
-      await Promise.all([a,b])
-      // This never reaches
-      console.log("onSuccess end")
+  return useMutation<void, undefined>(
+    async () => {
+      await delay();
+    },
+    {
+      onSuccess: async () => {
+        console.log("onSuccess start");
+        queryCache.setQueryData("notInstantiated", "hello");
+        queryCache.setQueryData("notInstantiated2", "hello");
+        const a = queryCache.refetchQueries("notInstantiated", { force: true });
+        const b = queryCache.refetchQueries("notInstantiated2", {
+          force: true,
+        });
+        console.log("promises are never resolved", a, b);
+        await Promise.all([a, b]);
+        // This never reaches
+        console.log("onSuccess end");
+      },
     }
-  })
+  );
 }
 
 export function delay(time: number = 1000): Promise<void> {
-  return new Promise((resolve) => setTimeout(resolve, time))
+  return new Promise((resolve) => setTimeout(resolve, time));
 }
